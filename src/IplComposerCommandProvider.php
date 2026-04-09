831f66c9d999bf292d71e65d833e661224e6ccce
Alexander Rieß <alexander.riess@icinga.com>
2026-04-09 14:04:53 +0200

    Rename load-assets command to assets

--- src/IplComposerCommandProvider.php
+++ src/IplComposerCommandProvider.php
@@ -3,14 +3,14 @@
 namespace ipl\Composer;
 
 use Composer\Plugin\Capability\CommandProvider as CommandProviderCapability;
-use ipl\Composer\Commands\AssetLoaderCommand;
+use ipl\Composer\Commands\AssetCommand;
 
 class IplComposerCommandProvider implements CommandProviderCapability
 {
     public function getCommands(): array
     {
         return [
-            new AssetLoaderCommand(),
+            new AssetCommand(),
         ];
     }
 }
