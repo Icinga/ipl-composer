d5d7bbca098ab8f677a3b697227d6c8258937c22
Alexander Rieß <alexander.riess@icinga.com>
2026-04-09 13:44:36 +0200

    Handle update and install events

--- src/IplComposerPlugin.php
+++ src/IplComposerPlugin.php
@@ -2,13 +2,16 @@
 
 namespace ipl\Composer;
 
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\Plugin\Capability\CommandProvider;
 use Composer\Plugin\Capable;
 use Composer\Plugin\PluginInterface;
 use Composer\Composer;
 use Composer\IO\IOInterface;
+use Composer\Script\Event;
+use ipl\Composer\Service\AssetMirror;
 
-class IplComposerPlugin implements PluginInterface, Capable
+class IplComposerPlugin implements PluginInterface, Capable, EventSubscriberInterface
 {
     public function activate(Composer $composer, IOInterface $io)
     {
@@ -30,5 +33,25 @@ class IplComposerPlugin implements PluginInterface, Capable
         return [
             CommandProvider::class => IplComposerCommandProvider::class,
         ];
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'post-install-cmd' => 'onPostInstall',
+            'post-update-cmd' => 'onPostUpdate',
+        ];
+    }
+
+    public function onPostInstall(Event $event): void
+    {
+        $event->getIO()->write('Mirroring asset directory');
+        AssetMirror::mirror($event->getComposer());
+    }
+
+    public function onPostUpdate(Event $event): void
+    {
+        $event->getIO()->write('Mirroring asset directory');
+        AssetMirror::mirror($event->getComposer());
     }
 }
