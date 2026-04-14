7f39c23e1f8110655ab5ffd428971763e9843744
Alexander Rieß <alexander.riess@icinga.com>
2026-04-14 10:31:35 +0200

    add snapshot command

--- src/Service/BuildService.php
+++ src/Service/BuildService.php
@@ -18,18 +18,23 @@ class BuildService
         string $version,
         bool $checkout,
         bool $tag,
-    ): int {
+    ): bool {
         $tags = $this->git->getTags();
         if (in_array($version, $tags)) {
             $this->output->writeln("Version $version has already been tagged!", OutputInterface::VERBOSITY_NORMAL);
-            return 1;
+            return false;
+        }
+
+        if(! $this->composerValidate()) {
+            $this->output->writeln("Composer validate failed", OutputInterface::VERBOSITY_NORMAL);
+            return false;
         }
 
         if ($checkout) {
             $branchName = "stable/$version";
             if (! $this->git->createBranchAndSwitch($branchName)) {
                 $this->output->writeln("Version branch $branchName already exists", OutputInterface::VERBOSITY_NORMAL);
-                return 1;
+                return false;
             }
         } else {
             $branchName = $this->git->getCurrentBranch();
@@ -38,26 +43,20 @@ class BuildService
 
         if (! is_dir('vendor')) {
             $this->output->writeln('No vendor directory found.', OutputInterface::VERBOSITY_NORMAL);
-            return 1;
+            return false;
         }
-
-        $application = new Application();
-        $application->setAutoExit(false);
 
         $this->git->add('vendor');
         $this->git->add('asset/*');
 
         if (! file_put_contents('VERSION', "v$version")) {
             $this->output->writeln("Could not write version file", OutputInterface::VERBOSITY_NORMAL);
-            return 1;
+            return false;
         }
 
         $this->git->add('VERSION');
 
         $this->git->commit("Version v$version");
-
-        $in = new ArrayInput(['command' => 'validate']);
-        $application->run($in, $this->output);
 
         if ($tag) {
             $this->git->tag("v$version", "Version v$version");
@@ -78,6 +77,17 @@ class BuildService
             OutputInterface::VERBOSITY_NORMAL,
         );
 
-        return 0;
+        return true;
+    }
+
+    protected function composerValidate(): bool
+    {
+        $application = new Application();
+        $application->setAutoExit(false);
+
+        $in = new ArrayInput(['command' => 'validate', '--no-check-all' => true, '--strict' => true]);
+        $code = $application->run($in, $this->output);
+
+        return $code === 0;
     }
 }
