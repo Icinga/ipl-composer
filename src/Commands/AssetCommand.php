3edb32e999a6a490093319799c0d61def8b26094
Alexander Rieß <alexander.riess@icinga.com>
2026-05-11 10:05:20 +0200

    Adjust description

--- src/Commands/AssetCommand.php
+++ src/Commands/AssetCommand.php
@@ -5,6 +5,7 @@ namespace ipl\Composer\Commands;
 use Composer\Command\BaseCommand;
 use ipl\Composer\Service\AssetMirror;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class AssetCommand extends BaseCommand
@@ -12,8 +13,18 @@ class AssetCommand extends BaseCommand
     protected function configure(): void
     {
         $this->setName('assets')
-            ->setDescription('Collect and load assets from various asset directories')
-            ->addOption("copy", "c", null, "Copy assets instead of creating symlinks");
+            ->setDescription(
+                'Collect and load assets from various asset directories. '
+                . 'By default the contents of the "asset" directory in each packages root directory are collected, '
+                . 'merged and placed into the "asset" directory of the application root directory. '
+                . 'This behavior can be modified by specifying additional data in the composer.json file.',
+            )
+            ->addOption(
+                'copy',
+                'c',
+                InputOption::VALUE_NONE,
+                'Copy assets instead of creating symlinks',
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
